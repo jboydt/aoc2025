1127bb91b9ea8e52c08379a648911dfd4e6d2464
Boyd Trolinger <boyd.trolinger@gmail.com>
2025-12-04 08:28:25 -0600

    Day 3 clippy lint

--- src/day/three.rs
+++ src/day/three.rs
@@ -3,8 +3,8 @@ pub fn solve(lines: &Vec<String>) -> (i64, i64) {
     let mut solution_one: i64 = 0;
     let mut solution_two: i64 = 0;
     for battery_bank in lines {
-        solution_one += get_joltage(&battery_bank, 2, 0);
-        solution_two += get_joltage(&battery_bank, 12, 0);
+        solution_one += get_joltage(battery_bank, 2, 0);
+        solution_two += get_joltage(battery_bank, 12, 0);
     }
 
     (solution_one, solution_two)
@@ -46,10 +46,10 @@ fn get_joltage(battery_str: &str, needed: usize, sum: i64) -> i64 {
         }
         counter += 1;
     }
-    let max_scaled = (10 as i64).pow(needed as u32 - 1) * max as i64;
+    let max_scaled = (10_i64).pow(needed as u32 - 1) * max;
     if needed == 1 {
-        return sum + max;
+        sum + max
     } else {
-        return sum + max_scaled + get_joltage(&battery_str[max_index + 1..], needed - 1, sum);
+        sum + max_scaled + get_joltage(&battery_str[max_index + 1..], needed - 1, sum)
     }
 }
